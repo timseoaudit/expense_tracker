619469d502da5179319d10f34747c6ed3b1edf21
Timneji <139955017+timseoaudit@users.noreply.github.com>
2024-05-09 00:10:37 +0100

    deleteing Expenses
    Now able to delete and recover expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -40,14 +40,50 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _addExpense(Expense expense) {
-    setState( () {
+    setState(() {
       _registeredExpenses.add(expense);
-    }
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+       SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(
+              () {
+                _registeredExpenses.insert(expenseIndex, expense);
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    //Empty Screen
+    Widget mainContent = const Center(
+      child: Text('No expenses logged yet. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
@@ -62,7 +98,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The Chart'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: mainContent,
           ),
         ],
       ),
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -60,7 +60,7 @@ class _NewExpenseState extends State<NewExpense> {
         amount: enteredAmount,
         date: _selectedDate!,
         category: _selectedCategory));
-        Navigator.pop(context);
+    Navigator.pop(context);
   }
 
   //Dispose Function
