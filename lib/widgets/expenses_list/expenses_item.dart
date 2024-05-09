ed326834ee1bd0ca5b7a5859fa4c9911777b1c2b
Timneji <139955017+timseoaudit@users.noreply.github.com>
2024-05-09 02:57:05 +0100

    Theming of App, Chart Implementation
    Implement Chart, add Theming for light mode and dark mode

--- lib/widgets/expenses_list/expenses_item.dart
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -12,8 +12,14 @@ class ExpenseItem extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title), // need styling
+            Text(
+              expense.title,
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                fontSize: 16.5,
+              ),
+            ),
             const SizedBox(height: 4),
             Row(
               children: [
@@ -21,8 +27,8 @@ class ExpenseItem extends StatelessWidget {
                 const Spacer(),
                 Row(
                   children: [
-                    Icon(categoryIcons[expense.category]), 
-                    const SizedBox(width: 8), 
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 8),
                     Text(expense.formattedDate),
                   ],
                 ),
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -18,6 +18,10 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.80),
+          margin: Theme.of(context).cardTheme.margin,
+        ),
         onDismissed: (direction) {
           onRemoveExpense(
             expenses[index],
